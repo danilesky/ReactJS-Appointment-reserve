221aeae5dfefce02edce362c6daee6c1e3641d6f
Daniel Skysľak <danilesky17@gmail.com>
2022-10-03 15:03:32 +0200

    Seting calendar tiles functions with moment js

--- src/components/calendar/Calendar/Calendar.js
+++ src/components/calendar/Calendar/Calendar.js
@@ -1,8 +1,38 @@
 import React from "react";
 import { Calendar as DatePicker } from "react-calendar";
 
+//24071998 - dateDay data
+
+const appointments = [
+  {
+    day: "04102022",
+    free: [1, 2],
+  },
+  {
+    day: "05102022",
+    free: [1, 2, 3, 4, 5, 6],
+  },
+  {
+    day: "06102022",
+    free: [1, 2, 3, 4],
+  },
+  {
+    day: "07102022",
+    free: [1, 2, 3, 4, 5],
+  },
+];
+
 const Calendar = () => {
-  return <DatePicker />;
+  //Todays date
+  const currentDate = new Date();
+  //Function that disables past day tiles
+  const disableTiles = ({ activeStartDate, date, view }) => {
+    return date < new Date();
+  };
+
+  //Function to generate number of FREE APPOINTMENTS for each day under DAYtileContent
+  const tileContent = ({ date }) => {};
+  return <DatePicker tileDisabled={disableTiles} tileContent={tileContent} />;
 };
 
 export default Calendar;
--- src/screens/Booking/Booking.screen.js
+++ src/screens/Booking/Booking.screen.js
@@ -1,14 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import Calendar from "../../components/calendar/Calendar/Calendar";
 import { ServicesContext } from "../../services/Services/services.context";
+import "react-calendar/dist/Calendar.css";
 
 const Booking = () => {
   const { services } = useContext(ServicesContext);
+
+  //disable past days
+
   return (
     <div>
       Booking
       <Calendar />
+      <Link to="/">Booking</Link>
     </div>
   );
 };
